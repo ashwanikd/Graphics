b94f3456dcc109346482e28f568de583b574f8e3
ashwani <b160137@nitsikkim.ac.in>
2020-04-11 02:06:53 +0530

    bootstrap fixed and redundant code redefined

---
+++ header/header.js
@@ -0,0 +1,18 @@
+var root = 'ashwanikd.github.io/Graphics/';
+document.write('<div class=\"container text-center\">\r\n '+
+               '<h1>Learning From Graphics<\/h1>\r\n'+
+               '<\/div>\r\n'+
+               '\r\n'+
+               '<div class=\"container nav-scroller py-1 mb-2 bg-dark\">\r\n'+
+               '<nav class=\"nav d-flex justify-content-between\">\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'index.html\">Home<\/a>\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'News\/index.html\">News & Random fun<\/a>\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'Drawing\/index.html\">Drawing<\/a>\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'Games\/index.html\">Games<\/a>\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'Fractals\/index.html\">Fractals<\/a>\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'Mathematics\/index.html\">Mathematics<\/a>\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'Physics\/index.html\">Physics<\/a>\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'Algorithms\/index.html\">Algorithms<\/a>\r\n'+
+               '<a class=\"p-4 text-light\" href=\"'+root+'About\/index.html\">About us<\/a>\r\n'+
+               '<\/nav>\r\n'+
+               '<\/div>');
